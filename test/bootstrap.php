34b000ba5b9c6df4722b4ff61da0b81627aa0db5
Daniel González Cerviño <daniel.gonzalez@ideup.com>
2012-09-04 15:51:40 +0200

    improved gitignore

---
+++ test/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This file is part of the D2Cache proyect.
+ * 
+ * Description of autoload
+ *
+ * @author : Daniel González Cerviño <daniel.gonzalez@ideup.com>
+ * @file : autoload.php , UTF-8
+ * @date : Sep 4, 2012 , 3:43:12 PM
+ */
+
+$loader = require_once __DIR__ . '../vendor/autoload.php';
