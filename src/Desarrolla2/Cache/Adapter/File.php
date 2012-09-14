6e8eb51bf82d385e8adea894f4d8ee67a00ac123
Daniel González Cerviño <daniel.gonzalez@ideup.com>
2012-09-14 12:52:59 +0200

    File Cache

--- src/Desarrolla2/Cache/Adapter/File.php
+++ src/Desarrolla2/Cache/Adapter/File.php
@@ -9,11 +9,101 @@
  * @file : File.php , UTF-8
  * @date : Sep 4, 2012 , 1:00:09 AM
  */
-class File
+
+namespace Desarrolla2\Cache\Adapter;
+
+use Desarrolla2\Cache\Adapter\AdapterInterface;
+use Desarrolla2\Cache\Exception\FileCacheException;
+
+class File implements AdapterInterface
 {
-   
+
     /**
      * @var int
      */
     protected $ttl;
+
+    public function __construct()
+    {
+        $cacheFile = $this->getCacheFile($key);
+        if (!file_put_contents($cacheFile, serialize($data))) {
+            throw new FileCacheException('Error saving data with the key ' . $key . ' to the cache file.');
+        }
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function delete($key)
+    {
+        if ($this->has($key)) {
+            $cacheFile = $this->getCacheFile($key);
+            if (!unlink($cacheFile)) {
+                throw new FileCacheException('Error deleting the file cache with key ' . $key);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function get($key)
+    {
+        if ($this->has($key)) {
+            $cacheFile = $this->getCacheFile($key);
+            if (!$data = unserialize(file_get_contents($cacheFile))) {
+                throw new FileCacheException('Error reading data with the key ' . $key . ' from the cache file.');
+            }
+            return $data;
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function has($key)
+    {
+        $cacheFile = $this->getCacheFile($key);
+        return file_exists($cacheFile);
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function set($key, $value, $ttl = null)
+    {
+        $cacheFile = $this->getCacheFile($key);
+        if (!file_put_contents($cacheFile, serialize($data))) {
+            throw new FileCacheException('Error saving data with the key ' . $key . ' to the cache file.');
+        }
+        return $this;
+    }
+
+    /**
+     * Get the specified cache file
+     */
+    protected function getCacheFile($key)
+    {
+        return $this->_cacheDir . DIRECTORY_SEPARATOR . strtolower($key) . '.cache';
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function setDefaultTtl($ttl)
+    {
+        
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function setOption($key, $value)
+    {
+        
+    }
+
 }
---
+++ src/Desarrolla2/Cache/Exception/FileCacheException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Cache proyect.
+ * 
+ * Description of ApcCacheException
+ *
+ * @author : Daniel González Cerviño <daniel.gonzalez@ideup.com>
+ * @file : ApcCacheException.php , UTF-8
+ * @date : Sep 14, 2012 , 12:17:39 PM
+ */
+
+namespace Desarrolla2\Cache\Exception;
+
+class FileCacheException extends \Exception
+{
+    
+}
