947fc4843e973d882f3ac9b4134cdb42fd5e3849
Daniel González Cerviño <daniel.gonzalez@ideup.com>
2012-09-14 12:20:33 +0200

    add APC cache adapter

--- src/Desarrolla2/Cache/Adapter/Apc.php
+++ src/Desarrolla2/Cache/Adapter/Apc.php
@@ -9,11 +9,69 @@
  * @file : Apc.php , UTF-8
  * @date : Sep 4, 2012 , 1:00:27 AM
  */
-class Apc
+use Desarrolla2\Cache\Adapter\AdapterInterface;
+use Desarrolla2\Cache\Exception\ApcCacheException;
+
+class Apc implements AdapterInterface
 {
- 
+
     /**
      * @var int
      */
     protected $ttl;
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function delete($key)
+    {
+        if ($this->exists($key)) {
+            if (!apc_delete(strtolower($key))) {
+                throw new ApcCacheException('Error deleting data with the key ' . $key . ' from the APC cache.');
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function get($key)
+    {
+        if ($this->exists($key)) {
+            if (!$data = apc_fetch(strtolower($key))) {
+                throw new ApcCacheException('Error fetching data with the key ' . $key . ' from the APC cache.');
+            }
+            return $data;
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function has($key)
+    {
+        return (boolean) apc_exists(strtolower($key));
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function set($key, $value, $ttl = null)
+    {
+        if (!apc_store(strtolower($key), $data)) {
+            throw new ApcCacheException('Error saving data with the key ' . $key . ' to the APC cache.');
+        }
+    }
+
+    /**
+     * {@inheritdoc } 
+     */
+    public function setDefaultTtl($ttl)
+    {
+        $this->ttl = $ttl;
+    }
+
 }
