17ebda237305b69543ca1a42d4d63c453b2f104a
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2012-10-03 00:23:38 +0200

    add travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,4 @@
  * @file : autoload.php , UTF-8
  * @date : Sep 4, 2012 , 3:43:12 PM
  */
-
-$loader = require_once __DIR__ . '/../vendor/autoload.php'; 
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
