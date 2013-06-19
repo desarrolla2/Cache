9e223757439760728205a2afda9dbf557c27ea86
Paul Dixon <pdixon@astreetpress.com>
2013-06-19 13:45:58 +0100

    refactored performance test so the the md5 hashing of test data was done outside the timing loops, added a test for has+get combinations, and pretty-printed the test results

--- tests/performance/common.php
+++ tests/performance/common.php
@@ -16,17 +16,38 @@
 
 use Desarrolla2\Timer\Timer;
 
+//build test data outside of timing loop
+$data=array();
+for ($i = 1; $i <= 10000; $i++) {
+    $data[$i]=md5($i);
+}
+
+
 $timer = new Timer();
 for ($i = 1; $i <= 10000; $i++) {
-    $cache->set(md5($i), md5($i), 3600);
+    $cache->set($data[$i], $data[$i], 3600);
 }
 $timer->mark('10.000 set');
 for ($i = 1; $i <= 10000; $i++) {
-    $cache->has(md5($i));
+    $cache->has($data[$i]);
 }
 $timer->mark('10.000 has');
 for ($i = 1; $i <= 10000; $i++) {
-    $cache->get(md5($i));
+    $cache->get($data[$i]);
 }
 $timer->mark('10.000 get');
-var_dump($timer->get());
+for ($i = 1; $i <= 10000; $i++) {
+    $cache->has($data[$i]);
+    $cache->get($data[$i]);
+}
+$timer->mark('10.000 has+get combos');
+
+$benchmarks=$timer->get();
+foreach ($benchmarks as $benchmark) {
+    printf(
+        "%30s : duration %0.2fms memory %s\n",
+        $benchmark['text'],
+        $benchmark['from_previous']*1000,
+        $benchmark['memory']
+    );
+}
