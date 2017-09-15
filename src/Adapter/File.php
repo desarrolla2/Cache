66271c82067433f2341930f592e434458f4e1743
soseki <soseki_m@allgrow.co.jp>
2017-09-15 20:58:27 +0900

    2017/09/15 add cache clear

--- src/Adapter/File.php
+++ src/Adapter/File.php
@@ -176,4 +176,20 @@ class File extends AbstractAdapter
     {
         return (time() > $ttl);
     }
+
+    public function cacheClear()
+    {
+      $files = glob($this->cacheDir.'{*'.self::CACHE_FILE_SUBFIX.'}',GLOB_BRACE);
+      $count = count($files);
+      for($i = 0 ; $i < $count ; $i++)
+      {
+        $data = $this->unpack(file_get_contents($files[$i]));
+        if($this->ttlHasExpired($data['ttl']))
+        {
+          $this->deleteFile($files[$i]);
+        }
+        unset($files[$i]);
+      }
+      return true;
+    }  
 }
